47d1a1101775bb2b28ce4c1840be6623852b6588
Eddie Garcia <edd1e234g@gmail.com>
2020-03-01 00:30:22 -0700

    Stuff

--- project/demos/button_demo/stateMachines.c
+++ project/demos/button_demo/stateMachines.c
@@ -5,10 +5,33 @@
 
 char toggle() {
   static char state = 0;
+
+  switch(state) {
+  case 0:
+    red_on = 0;
+    green_on = 0;
+    state = 1;
+    break;
+  case 1:
+    red_on = 1;
+    state = 2;
+    break;
+  case 2:
+    red_on = 0;
+    green_on = 1;
+    state = 3;
+    break;
+  case 3:
+    red_on = 1;
+    green_on = 1;
+    state = 0;
+    break;
+  }
+  return 1;
 }
 
 void state_advance()
 {
   led_changed = toggle();
-  led_update(); 
+  led_update();
 }
