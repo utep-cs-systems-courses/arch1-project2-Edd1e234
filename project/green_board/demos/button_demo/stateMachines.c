ce55912e5f907cabb1c52cde8f19140d97be31a6
Eddie Garcia <edd1e234g@gmail.com>
2020-03-18 13:14:06 -0600

    New periods

--- project/green_board/demos/button_demo/stateMachines.c
+++ project/green_board/demos/button_demo/stateMachines.c
@@ -4,7 +4,7 @@
 #include "switches.h"
 #include "buzzer.h"
 
-char state = 0; 
+char state = 0;
 char state_button_1 = 0;  // This will only be changed by the switch interrupt handler.
 char state_button_2 = 0;
 char state_button_3 = 0;
@@ -16,28 +16,28 @@ static int eighth_note = 35;
 
 /*First part ends at 16.*/
 int periods[] = {758, 758, 758, 1517, 803, 803, 1607, 902, 902, 902, 1804, 1136, 1136, 758,
-		 4816, 4900, 4816, 4050, // Set 17
-		 4816, 4900, 4816, 4050,
-		 4816, 4900, 4816, 4050, // Set 18
-		 4816, 4900, 4816, 4050, // This is also 30.
-		 4816, 4900, 4816, 4050, // Set 19
-		 4816, 4900, 4816, 4050,
-		 4816, 4900, 4816, 4050, // Set 20
-		 4816, 4900, 4816, 4050, // 46
-		 4816, 4900, 4816, 4050, // Set 21
-		 4816, 4900, 4816, 4050, // 54
-		 4816, 4900, 4816, 4050, // Set 22
-		 4816, 4900, 4816, 4050, // 58
-		 4816, 4900, 4816, 4050, // Set 23
-		 4816, 4900, 4816, 4050, // 62
-		 4816, 4900, 4816, 4050, // Set 24
-		 4816, 4900, 4816, 4050  // 66
-		 
+		 4816, 6428, 4816, 4050, // Set 17
+		 4816, 6428, 4816, 4050,
+		 4816, 6428, 4816, 4050, // Set 18
+		 4816, 6428, 4816, 4050, // This is also 30.
+		 4816, 6428, 4816, 4050, // Set 19
+		 4816, 6428, 4816, 4050,
+		 4816, 6428, 4816, 4050, // Set 20
+		 4816, 6428, 4816, 4050, // 46
+		 4816, 6428, 4816, 4050, // Set 21
+		 4816, 6428, 4816, 4050, // 54
+		 4816, 6428, 4816, 4050, // Set 22
+		 4816, 6428, 4816, 4050, // 58
+		 4816, 6428, 4816, 4050, // Set 23
+		 4816, 6428, 4816, 4050, // 62
+		 4816, 6428, 4816, 4050, // Set 24
+		 4816, 6428, 4816, 4050  // 66
+
 };
 // First set is 14.
 int time_set[] = {188, 188, 188, 188, 188, 188, 188, 188, 188, 188, 188, 188, 188, 188,
 		  43, 43, 43, 43, // Set 17
-		  43, 43, 43, 43, 
+		  43, 43, 43, 43,
 		  43, 43, 43, 43, // Set 18
 		  43, 43, 43, 43,
 		  43, 43, 43, 43, // Set 19
@@ -56,13 +56,21 @@ int time_set[] = {188, 188, 188, 188, 188, 188, 188, 188, 188, 188, 188, 188, 18
 
 static int periods_inv[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 			    5000, 0, 0, 0, // Set 17
-			    758, 0, 0, 0, 
+			    758, 0, 0, 0,
 			    5000, 0, 0, 0, // Set 18
 			    758, 0, 0, 0,
 			    5000, 0, 0, 0, // Set 19
 			    758, 0, 0, 0,
 			    5000, 0, 0, 0, // Set 20
 			    1517, 0, 0, 0,
+					5000, 0, 0, 0, // Set 21
+					803, 0, 0, 0,
+					5000, 0, 0, 0, // Set 22
+					803, 0, 0, 0,
+					5000, 0, 0, 0, // Set 23
+					803, 0, 0, 0,
+					5000, 0, 0, 0, // Set 24
+					1607, 0, 0, 0
 };
 int value = 5;
 
@@ -106,13 +114,13 @@ char toggle_button_2() {
     if (state_button_2) {
       green_led_state = 1;
       red_led_state = 0;
-      state_button_2 = 0; 
+      state_button_2 = 0;
     } else {
       red_led_state = 1;
       green_led_state = 0;
       state_button_2 = 1;
     }
-  return 1; 
+  return 1;
 }
 
 char toggle_button_3() {
@@ -120,12 +128,12 @@ char toggle_button_3() {
   case 0:
     green_led_state = 0;
     red_led_state = 0;
-    state_button_3 = 1; 
+    state_button_3 = 1;
     break;
   case 1:
     green_led_state = 0;
     red_led_state = 0;
-    state_button_3 = 2; 
+    state_button_3 = 2;
     break;
   case 2:
     green_led_state = 0;
@@ -158,7 +166,7 @@ char toggle_button_4() {
     case 0:
       buzzer_set_period(periods[value]);
       state_button_4 = 1;
-      turn_off(); 
+      turn_off();
       break;
     case 1:
       buzzer_set_period(periods_inv[value]);
@@ -175,7 +183,7 @@ void state_advance()
 {
   switch (state) {
   case 0:
-    value = 0; 
+    value = 0;
     buzzer_set_period(0);
     leds_changed = toggle_button_1();
     break;
@@ -185,7 +193,7 @@ void state_advance()
     leds_changed = toggle_button_2();
     break;
   case 2:
-    value = 0; 
+    value = 0;
     buzzer_set_period(0);
     leds_changed = toggle_button_3();
     break;
