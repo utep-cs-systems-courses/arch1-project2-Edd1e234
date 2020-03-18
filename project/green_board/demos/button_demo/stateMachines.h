6a3d63f56fb514cdd5ab6b3b15d77fd44ad09a6e
student <edd1e234@gmail.com>
2020-03-18 17:10:23 -0600

    Fixed ram leakage

--- project/green_board/demos/button_demo/stateMachines.h
+++ project/green_board/demos/button_demo/stateMachines.h
@@ -3,7 +3,7 @@
 
 // These should only be changed in switches.c or maybe interrupt function
 
-extern int periods[], time_set[];
+extern const int periods[], periods_inv[], time_set[];
 extern int value;
 extern char state, state_button_1, state_button_2, state_button_3, state_button_4;
 static char FINAL; 
