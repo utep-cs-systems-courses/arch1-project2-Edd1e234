1015bdd661490a736009263541838d31debba741
student <edd1e234@gmail.com>
2020-03-20 22:09:33 -0600

    Changed file name

--- project/green_board/demos/button_demo/switches.c
+++ project/green_board/demos/button_demo/switches.c
@@ -9,11 +9,11 @@ char switch_state_down_button_4, switch_state_changed; /* effectively boolean */
 static char 
 switch_update_interrupt_sense()
 {
-  char p1val = P2IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
-  P2IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P2IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-  return p1val;
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
 }
 
 void 
@@ -31,12 +31,12 @@ switch_init()			/* setup switch */
 void
 switch_interrupt_handler()
 {
-  char p1val = switch_update_interrupt_sense();
+  char p2val = switch_update_interrupt_sense();
   
-  switch_state_down_button_1 = (p1val & SW1) ? 0: 1;
-  switch_state_down_button_2 = (p1val & SW2) ? 0: 1;
-  switch_state_down_button_3 = (p1val & SW3) ? 0: 1;
-  switch_state_down_button_4 = (p1val & SW4) ? 0: 1;
+  switch_state_down_button_1 = (p2val & SW1) ? 0: 1;
+  switch_state_down_button_2 = (p2val & SW2) ? 0: 1;
+  switch_state_down_button_3 = (p2val & SW3) ? 0: 1;
+  switch_state_down_button_4 = (p2val & SW4) ? 0: 1;
 
   if (switch_state_down_button_1) {
     state_button_1 = 0;           /* This is to reset the binary state machine.*/
