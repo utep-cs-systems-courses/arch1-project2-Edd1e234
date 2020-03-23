439c452fb58a952ab778afcb34c67fc8e1dde197
student <edd1e234@gmail.com>
2020-03-22 18:26:04 -0600

    Final

--- project/green_board/demos/button_demo/p2_interrupt_handler.c
+++ project/green_board/demos/button_demo/p2_interrupt_handler.c
@@ -4,7 +4,7 @@
 /* Switch on P2 (S2) */
 void
 __interrupt_vec(PORT2_VECTOR) Port_2(){
-  if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+  if (P2IFG & SWITCHES) {  
     P2IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
     switch_interrupt_handler();	/* single handler for all switches */
   }
